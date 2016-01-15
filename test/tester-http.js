3b759760d6802574bf7a10c0b6601a5f04b1ddb7
Hyunje Alex Jun <me@noraesae.net>
2016-01-15 19:27:20 +0900

    Implement basic HTTP request methods in Tester

---
+++ test/tester-http.js
@@ -0,0 +1,69 @@
+'use strict';
+
+const bloodyTester = require('../');
+const test = require('ava');
+const testServer = require('./helpers/test-server');
+
+let server;
+test.before.cb(t => {
+  server = testServer.listen(1234, t.end);
+});
+
+test.after.cb(t => {
+  server.close(t.end);
+});
+
+test('send GET request', async t => {
+  let tester = bloodyTester.createTester(1234);
+  const res = await tester.get('/');
+
+  t.is(res.data, 'hello, get');
+});
+
+test('send POST request', async t => {
+  let tester = bloodyTester.createTester(1234);
+  const res = await tester.post('/');
+
+  t.is(res.data, 'hello, post');
+});
+
+test('send PUT request', async t => {
+  let tester = bloodyTester.createTester(1234);
+  const res = await tester.put('/');
+
+  t.is(res.data, 'hello, put');
+});
+
+test('send DELETE request', async t => {
+  let tester = bloodyTester.createTester(1234);
+  const res = await tester.delete('/');
+
+  t.is(res.data, 'hello, delete');
+});
+
+test('send request with query string', async t => {
+  let tester = bloodyTester.createTester(1234);
+  const res = await tester.get('/query?a=10&b=20');
+
+  t.same(res.data, {a: '10', b: '20'});
+});
+
+test('send request with query object', async t => {
+  let tester = bloodyTester.createTester(1234);
+  const res = await tester.get('/query', {a: '10', b: '20'});
+
+  t.same(res.data, {a: '10', b: '20'});
+});
+
+test('send request with body', async t => {
+  let tester = bloodyTester.createTester(1234);
+  const res = await tester.post('/body', {a: 10, b: 20});
+
+  t.same(res.data, {a: 10, b: 20});
+});
+
+test('send request with full path', async t => {
+  let tester = bloodyTester.createTester(1234);
+  const res = await tester.get('http://localhost:1234');
+  t.same(res.data, 'hello, get');
+});
