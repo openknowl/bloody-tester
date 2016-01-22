e70ae6ee854d069434aa0a5b3bce2691b86baf28
Hyunje Alex Jun <me@noraesae.net>
2016-01-22 17:46:00 +0900

    Add Lazy module
    The Lazy module is to refer to another actor's attribute when creating
    an actor. If an attribute is defined with `lazy('actor', 'attr')`, the
    value will be actually retrieved when creating the actor.

--- index.js
+++ index.js
@@ -1,5 +1,8 @@
 'use strict';
 
+const Lazy = require('./lib/lazy');
 const Tester = require('./lib/tester');
 
 exports.createTester = (host) => new Tester(host);
+
+exports.lazy = Lazy.createLazy;
---
+++ test/lazy.js
@@ -0,0 +1,34 @@
+'use strict';
+
+const bloodyTester = require('../');
+const Person = require('./helpers/actors/person');
+const test = require('ava');
+
+const lazy = bloodyTester.lazy;
+
+let tester = bloodyTester.createTester(1234);
+test.afterEach(async () => {
+  await tester.cleanup();
+});
+
+test('use an actor\'s id attribute lazily', async t => {
+  let scene = await tester.createScene();
+  await scene.createActor('guido', Person, {id: 12341234, name: 'Guido van Rossum', parent: null});
+  await scene.createActor('james', Person, {id: 12341235, name: 'James Gosling', parent: lazy('guido')});
+  t.same(scene.getActor('james'), {id: 12341235, name: 'James Gosling', parent: 12341234});
+});
+
+test('use an actor\'s any attribute lazily', async t => {
+  let scene = await tester.createScene();
+  await scene.createActor('guido', Person, {id: 12341234, name: 'Guido van Rossum', parentName: null});
+  await scene.createActor('james', Person, {id: 12341235, name: 'James Gosling', parentName: lazy('guido', 'name')});
+  t.same(scene.getActor('james'), {id: 12341235, name: 'James Gosling', parentName: 'Guido van Rossum'});
+});
+
+test('with initialization', async t => {
+  let scene = await tester.createScene([
+    {id: 'guido', type: Person, instance: {id: 12341234, name: 'Guido van Rossum', parent: null}},
+    {id: 'james', type: Person, instance: {id: 12341235, name: 'James Gosling', parent: lazy('guido')}}
+  ]);
+  t.same(scene.getActor('james'), {id: 12341235, name: 'James Gosling', parent: 12341234});
+});
